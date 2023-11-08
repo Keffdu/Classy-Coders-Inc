031218cecc8b67d9077fbb788c3e138cfc1008b9
Hector Pulido <87735459+hepulido@users.noreply.github.com>
2023-11-08 16:18:01 -0500

    Created salesPerson and softwareEngineer classes (#2)

--- classes/SalesPerson.js
+++ classes/SalesPerson.js
@@ -1,10 +1,25 @@
-class SalesPerson {
+const { Employees } = require("./Employees")
 
+class SalesPerson extends Employees {
+    #totalSales = 0;
+    constructor(name, title, salary, clients = []) {
+        super(name, title, salary,)
+        this.clients = clients;
+    }
 
-
-    
+    getSalesNumber() {
+        return this.#totalSales;
+    }
+    makeSale(amount) {
+        this.#totalSales += amount
+    }
 }
 
 module.exports = {
     SalesPerson,
 }
+
+
+
+
+
